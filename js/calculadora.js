342937758d4f08deff7a77bad11e7ce7781d5785
Lais Souza <lais_souza01@hotmail.com>
2023-03-07 20:51:56 -0300

    implementação da soma e subtração

--- js/calculadora.js
+++ js/calculadora.js
@@ -3,13 +3,14 @@ function somar() {
   const numero2 = parseFloat(document.getElementById("numero2Soma").value);
   // evite mexer no código acima!
 
-
+  const resultado = numero1 + numero2
+  
 
 
 
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
   Em seguida, ao invés de mostrar o numero 10, mostre o resultado da operação */
-  document.getElementById("resultadoSoma").innerHTML = 10;
+  document.getElementById("resultadoSoma").innerHTML = resultado;
 }
 
 function subtrair() {
@@ -17,13 +18,13 @@ function subtrair() {
   const numero2 = parseFloat(document.getElementById("numero2Sub").value);
   // evite mexer no código acima!
 
-
+  const resultadoSubtrair = numero1 - numero2
 
 
 
   /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
    Em seguida, ao invés de mostrar o numero 9, mostre o resultado da operação */
-  document.getElementById("resultadoSub").innerHTML = 9;
+  document.getElementById("resultadoSub").innerHTML = resultadoSubtrair;
 }
 
 function multiplicar() {
@@ -31,7 +32,7 @@ function multiplicar() {
   const numero2 = parseFloat(document.getElementById("numero2Mult").value);
   // evite mexer no código acima!
 
-
+  const resultadoMultiplicar = numero1 * numero2
 
 
 
